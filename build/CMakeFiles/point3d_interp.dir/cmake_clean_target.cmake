21eef68de31408231480fd89071cee465807063a
Joe Cheung <zhangchao.simzc@outlook.com>
2025-11-13 14:11:26 +0800

    Exclude build and cache directories from git tracking

--- build/CMakeFiles/point3d_interp.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libpoint3d_interp.a"
-)
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/joe/repo/point3d_interp
-# Build directory: /home/joe/repo/point3d_interp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("examples")
-subdirs("tests")
